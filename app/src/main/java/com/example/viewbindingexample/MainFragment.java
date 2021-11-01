bc297242106da9f5762a76dd7dd0e53f021a67e3
milaadd <wind.ws10@outlook.com>
2021-11-01 15:45:26 +0330

    Version 2.2.3 Code 23 Transfer Updated

--- app/src/main/java/com/example/viewbindingexample/MainFragment.java
+++ app/src/main/java/com/example/viewbindingexample/MainFragment.java
@@ -1,22 +1,18 @@
 package com.example.viewbindingexample;
 
-import android.os.Bundle;
 import android.view.View;
 
 import com.example.viewbindingexample.databinding.FragmentMainBinding;
 
 import ir.mich.genericviewbinder.FragmentBinder;
-import ir.mich.genericviewbinder.OpenFragment;
 
 public class MainFragment extends FragmentBinder<FragmentMainBinding> implements View.OnClickListener {
 
-    static boolean flag = false;
-    int color;
 
     @Override
     protected void onCreateView() {
 //        android:id="@+id/example_text"
-        color = transfer.getExtras().getInt("color");
+        int color = transfer.getExtras().getInt("color");
         binding.exampleText.setText("Color is " + hexColor(color));
         binding.btnTransfer.setOnClickListener(this);
         binding.root.setBackgroundColor(color);
@@ -28,20 +24,9 @@ public class MainFragment extends FragmentBinder<FragmentMainBinding> implements
 
     @Override
     public void onClick(View v) {
-        /*transfer.openSomeActivityForResult(
+        transfer.openSomeActivityForResult(
                 MainActivity.class, null, result -> {
                     binding.exampleText.setText("im Back from activity");
-                });*/
-        if (flag = !flag) {
-            Bundle bundle = new Bundle();
-            bundle.putInt("color", color+1);
-            transfer.openSomeFragmentForResult("key", (requestKey, result) -> {
-                toast(requestKey + " | " + result.getString("code"));
-            }, new OpenFragment(binding.root, new MainFragment(), null, null, bundle));
-        } else {
-            Bundle bundle = new Bundle();
-            bundle.putString("code", "im " + hexColor(color));
-            transfer.closeSomeFragmentForResult("key", bundle);
-        }
+                });
     }
 }
