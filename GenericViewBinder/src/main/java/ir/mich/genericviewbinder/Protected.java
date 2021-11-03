03b65ce140c9322e37119be5f00e248889a2a821
milaadd <wind.ws10@outlook.com>
2021-11-03 15:41:21 +0330

    Version 2.2.9 Code 29 Protected.class updated

--- GenericViewBinder/src/main/java/ir/mich/genericviewbinder/Protected.java
+++ GenericViewBinder/src/main/java/ir/mich/genericviewbinder/Protected.java
@@ -4,28 +4,28 @@ class Protected {
     private final Reserve reserve;
     private final String key;
 
-    public Protected(String key, Reserve reserve) {
+    protected Protected(String key, Reserve reserve) {
         this.key = key;
         this.reserve = reserve;
     }
 
-    public static Key invoke(Protected aProtected) {
+    protected static Key invoke(Protected aProtected) {
         return new Key(aProtected);
     }
 
-    public interface Reserve {
+    protected interface Reserve {
         void queue();
     }
 
-    static class Key {
+    protected static class Key {
 
         private final Protected aProtected;
 
-        public Key(Protected aProtected) {
+        protected Key(Protected aProtected) {
             this.aProtected = aProtected;
         }
 
-        public void key(String key) {
+        protected void key(String key) {
             if (aProtected != null) {
                 if (aProtected.key.equals(key)) {
                     aProtected.reserve.queue();
